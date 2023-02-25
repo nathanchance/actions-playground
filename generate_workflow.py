5c4fb52abb17792c2611e0f84c5c20508bab580f
Nathan Chancellor <nathan@kernel.org>
2023-02-25 16:06:25 -0700

    Import
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ generate_workflow.py
@@ -0,0 +1,281 @@
+#!/usr/bin/env python3
+
+import argparse
+import contextlib
+import hashlib
+from pathlib import Path
+import sys
+import yaml
+
+from utils import get_config_from_generator, get_llvm_versions, get_repo_ref, patch_series_flag, print_red
+
+
+def parse_args(trees):
+    parser = argparse.ArgumentParser(
+        description="Generate GitHub Action Workflow YAML.")
+    parser.add_argument("tree",
+                        help="The git repo and ref to filter in.",
+                        choices=[tree["name"] for tree in trees])
+    return parser.parse_args()
+
+
+def initial_workflow(name, cron, tuxsuite_yml, workflow_yml):
+    return {
+        "name": name,
+        "on": {
+            # https://docs.github.com/en/actions/reference/workflow-syntax-for-github-actions#onpushpull_requestpaths
+            "push": {
+                "branches": [
+                    # Allow testing on branches with a presubmit/ prefix
+                    "presubmit/*",
+                ],
+                "paths": [
+                    "check_logs.py",
+                    "utils.py",
+                    tuxsuite_yml,
+                    workflow_yml,
+                ],
+            },
+            # https://docs.github.com/en/free-pro-team@latest/actions/reference/events-that-trigger-workflows#scheduled-events
+            "schedule": [
+                {"cron": cron},
+            ],
+            # https://docs.github.com/en/free-pro-team@latest/actions/reference/events-that-trigger-workflows#workflow_dispatch
+            "workflow_dispatch": None,
+        },
+        "permissions": "read-all",
+        "jobs": {},
+    }  # yapf: disable
+
+
+def print_config(build):
+    config = build["config"]
+    if isinstance(config, list):
+        config_name = config[0]
+        i = 1
+        while i < len(config):
+            config_name += "+" + config[i]
+            i += 1
+    else:
+        config_name = config
+    return config_name
+
+
+def get_job_name(build):
+    job = "ARCH=" + (build["ARCH"] if "ARCH" in build else "x86_64")
+    # BOOT=1 is the default, only show if we have disabled it
+    if not build["boot"]:
+        job += " BOOT=0"
+    # LLVM=0 does not make much sense. Translate LLVM=0 into CC=clang
+    if build["llvm"]:
+        job += " LLVM=1"
+    else:
+        job += " CC=clang"
+    # If LD was specified, show what it is
+    if "make_variables" in build and "LD" in build["make_variables"]:
+        job += " LD=" + str(build["make_variables"]["LD"])
+    job += " LLVM_IAS=" + str(build["make_variables"]["LLVM_IAS"])
+    # Having "LLVM <VER>" is a little hard to parse, make it look like
+    # an environment variable
+    job += " LLVM_VERSION=" + str(build["llvm_version"])
+    job += " " + print_config(build)
+    return job
+
+
+def sanitize_job_name(name):
+    return "_" + hashlib.new("md5", name.encode("utf-8")).hexdigest()
+
+
+def tuxsuite_setups(job_name, tuxsuite_yml, repo, ref):
+    tuxsuite_yml_name = Path(tuxsuite_yml).name
+    # Input: '<tree>-clang-<num>.tux.yml'
+    # Output: [<tree_parts>, 'clang', <num>]
+    workflow_parts = tuxsuite_yml_name.replace('.tux.yml', '').split('-')
+
+    tree = '-'.join(workflow_parts[0:-2])
+    patch_series = patch_series_flag(tree)
+
+    ci_folder = Path(__file__).resolve().parent
+    with Path(ci_folder, 'LLVM_TOT_VERSION').open(encoding='utf-8') as file:
+        max_version = int(file.read())
+    llvm_version = workflow_parts[-1]
+    with contextlib.suppress(ValueError):
+        if int(llvm_version) == max_version:
+            llvm_version = 'nightly'
+    return {
+        f"cache_check_{job_name}": {
+            "name": f"cache check ({job_name})",
+            "runs-on": "ubuntu-latest",
+            "container": f"tuxmake/clang-{llvm_version}",
+            "steps": [
+                {
+                    "uses": "actions/checkout@v3",
+                },
+                {
+                    "name": "Should build run?",
+                    "run": ('if python3 should_run.py || { ret=$?; ( exit $ret ) }; then\n'
+                            '  echo "should_run=true" >>$GITHUB_OUTPUT\n'
+                            'else\n'
+                            '    case $ret in\n'
+                            '      2) echo "should_run=false" >>$GITHUB_OUTPUT ;;\n'
+                            '      *) exit 1 ;;\n'
+                            '    esac\n'
+                            'fi\n'),
+                    "env": {"GITHUB_TOKEN": '${{ secrets.GITHUB_TOKEN }}'},
+                },
+            ],
+        },
+        f"kick_tuxsuite_{job_name}": {
+            "name": f"TuxSuite ({job_name})",
+            "needs": f"cache_check_{job_name}",
+            "if": f"${{{{ needs.cache_check_{job_name}.outputs.should_run == 'true' }}}}",
+            # https://docs.github.com/en/free-pro-team@latest/actions/reference/workflow-syntax-for-github-actions#jobsjob_idruns-on
+            "runs-on": "ubuntu-latest",
+            "container": "tuxsuite/tuxsuite",
+            "env": {
+                "TUXSUITE_TOKEN": "${{ secrets.TUXSUITE_TOKEN }}",
+            },
+            "timeout-minutes": 480,
+            "steps": [
+                {
+                    "uses": "actions/checkout@v3",
+                },
+                {
+                    "name": "tuxsuite",
+                    "run": f"tuxsuite plan --git-repo {repo} --git-ref {ref} --job-name {job_name} --json-out builds.json {patch_series}{tuxsuite_yml} || true",
+                },
+                {
+                    "name": "save output",
+                    "uses": "actions/upload-artifact@v3",
+                    "with": {
+                        "path": "builds.json",
+                        "name": f"output_artifact_{job_name}",
+                        "if-no-files-found": "error",
+                    },
+                },
+            ],
+        },
+    }  # yapf: disable
+
+
+def get_steps(build, build_set):
+    name = get_job_name(build)
+    return {
+        sanitize_job_name(name): {
+            "runs-on": "ubuntu-latest",
+            "needs": f"kick_tuxsuite_{build_set}",
+            "name": name,
+            "env": {
+                "ARCH": build["ARCH"] if "ARCH" in build else "x86_64",
+                "LLVM_VERSION": build["llvm_version"],
+                "BOOT": int(build["boot"]),
+                "CONFIG": print_config(build),
+            },
+            "container": {
+                "image": "ghcr.io/clangbuiltlinux/qemu",
+                "options": "--ipc=host",
+            },
+            "steps": [
+                {
+                    "uses": "actions/checkout@v3",
+                    "with": {
+                        "submodules": True,
+                    },
+                },
+                {
+                    "uses": "actions/download-artifact@v3",
+                    "with": {
+                        "name": f"output_artifact_{build_set}",
+                    },
+                },
+                {
+                    "name": "Check Build and Boot Logs",
+                    "run": "./check_logs.py",
+                },
+            ],
+        },
+    }  # yapf: disable
+
+
+def get_cron_schedule(schedules, tree_name, llvm_version):
+    for item in schedules:
+        if item["name"] == tree_name and \
+           item["llvm_version"] == llvm_version:
+            return item["schedule"]
+    print_red(f"Could not find schedule for {tree_name} clang-{llvm_version}?")
+    sys.exit(1)
+
+
+def print_builds(config, tree_name, llvm_version):
+    repo, ref = get_repo_ref(config, tree_name)
+    toolchain = f"clang-{llvm_version}"
+    tuxsuite_yml = f"tuxsuite/{tree_name}-{toolchain}.tux.yml"
+    github_yml = f".github/workflows/{tree_name}-{toolchain}.yml"
+
+    check_logs_defconfigs = {}
+    check_logs_distribution_configs = {}
+    check_logs_allconfigs = {}
+    for build in config["builds"]:
+        if build["git_repo"] == repo and \
+           build["git_ref"] == ref and \
+           build["llvm_version"] == llvm_version:
+            cfg_str = str(build["config"])
+            if "defconfig" in cfg_str or "chromeos" in cfg_str:
+                check_logs_defconfigs.update(get_steps(build, "defconfigs"))
+            elif "https://" in cfg_str:
+                check_logs_distribution_configs.update(
+                    get_steps(build, "distribution_configs"))
+            else:
+                check_logs_allconfigs.update(get_steps(build, "allconfigs"))
+
+    workflow_name = f"{tree_name} ({toolchain})"
+    cron_schedule = get_cron_schedule(config["tree_schedules"], tree_name,
+                                      llvm_version)
+    workflow = initial_workflow(workflow_name, cron_schedule, tuxsuite_yml,
+                                github_yml)
+    workflow["jobs"].update(
+        tuxsuite_setups("defconfigs", tuxsuite_yml, repo, ref))
+    workflow["jobs"].update(check_logs_defconfigs)
+
+    if check_logs_distribution_configs:
+        workflow["jobs"].update(
+            tuxsuite_setups("distribution_configs", tuxsuite_yml, repo, ref))
+        workflow["jobs"].update(check_logs_distribution_configs)
+
+    if check_logs_allconfigs:
+        workflow["jobs"].update(
+            tuxsuite_setups("allconfigs", tuxsuite_yml, repo, ref))
+        workflow["jobs"].update(check_logs_allconfigs)
+
+    with open(github_yml, "w", encoding='utf-8') as file:
+        orig_stdout = sys.stdout
+        sys.stdout = file
+        print("# DO NOT MODIFY MANUALLY!")
+        print("# This file has been autogenerated by invoking:")
+        print(f"# $ ./generate_workflow.py {tree_name}")
+        print(
+            yaml.dump(workflow,
+                      Dumper=yaml.Dumper,
+                      width=1000,
+                      sort_keys=False))
+        sys.stdout = orig_stdout
+
+
+# https://github.com/yaml/pyyaml/issues/240
+def str_presenter(dumper, data):
+    """configures yaml for dumping multiline strings
+    Ref: https://stackoverflow.com/questions/8640959/how-can-i-control-what-scalar-form-pyyaml-uses-for-my-data"""
+    if data.count('\n') > 0:  # check for multiline string
+        return dumper.represent_scalar('tag:yaml.org,2002:str',
+                                       data,
+                                       style='|')
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data)
+
+
+yaml.add_representer(str, str_presenter)
+
+if __name__ == "__main__":
+    generated_config = get_config_from_generator()
+    args = parse_args(generated_config["trees"])
+    for llvm_ver in get_llvm_versions(generated_config, args.tree):
+        print_builds(generated_config, args.tree, llvm_ver)
--- utils.py
+++ utils.py
@@ -1,5 +1,194 @@
+import glob
+import json
+import os
+import pathlib
+import sys
+import yaml
+
+
+def get_config_from_generator():
+    # Trusted input.
+    # https://github.com/yaml/pyyaml/wiki/PyYAML-yaml.load(input)-Deprecation
+    try:
+        with open("generator.yml", encoding='utf-8') as file:
+            return yaml.load(file, Loader=yaml.FullLoader)
+    except FileNotFoundError as err:
+        print_red("generator.yml not found?")
+        raise err
+
+
+def get_image_name():
+    arch = os.environ["ARCH"]
+    if arch == "powerpc":
+        subarch = get_cbl_name()
+        return {
+            "ppc32": "uImage",
+            "ppc64": "vmlinux",
+            "ppc64le": "zImage.epapr",
+        }[subarch]
+    return {
+        "arm": "zImage",
+        "arm64": "Image.gz",
+        "i386": "bzImage",
+        "mips": "vmlinux",
+        "riscv": "Image",
+        "s390": "bzImage",
+        "um": "linux",
+        "x86_64": "bzImage",
+    }[arch]
+
+
+def get_cbl_name():
+    arch = os.environ["ARCH"]
+    full_config = os.environ["CONFIG"]
+    base_config = full_config.split("+")[0]
+
+    # Distribution configurations have a URL
+    if "https://" in base_config:
+        if "alpine" in base_config:
+            alpine_to_cbl = {
+                "aarch64": "arm64",
+                "armv7": "arm32_v7",
+                "riscv64": "riscv",
+                "x86_64": "x86_64",
+            }
+            # The URL is https://.../config-edge.<arch>
+            alpine_arch = base_config.split(".")[-1]
+            return alpine_to_cbl[alpine_arch]
+        if "fedora" in base_config:
+            fedora_to_cbl = {
+                "aarch64": "arm64",
+                "armv7hl": "arm32_v7",
+                "i686": "x86",
+                "ppc64le": "ppc64le",
+                "s390x": "s390",
+                "x86_64": "x86_64",
+            }
+            # The URL is https://.../kernel-<arch>-fedora.config
+            fedora_arch = base_config.split("/")[-1].split("-")[1]
+            return fedora_to_cbl[fedora_arch]
+        if "openSUSE" in base_config:
+            suse_to_cbl = {
+                "arm64": "arm64",
+                "armv7hl": "arm32_v7",
+                "i386": "x86",
+                "ppc64le": "ppc64le",
+                "riscv64": "riscv",
+                "s390x": "s390",
+                "x86_64": "x86_64",
+            }
+            # The URL is https://.../<arch>/default
+            suse_arch = base_config.split("/")[-2]
+            return suse_to_cbl[suse_arch]
+        # Arch Linux is x86_64 only
+        if "archlinux" in base_config:
+            return "x86_64"
+
+    # ChromeOS configurations have the architecture as the second to last
+    # folder of the second config fragment path:
+    # chromeos/config/chromeos/arm64/common.config
+    # chromeos/config/chromeos/x86_64/common.config
+    if "chromeos" in base_config:
+        return full_config.split("+")[1].split("/")[-2]
+
+    unique_defconfigs = {
+        "multi_v5_defconfig": "arm32_v5",
+        "aspeed_g5_defconfig": "arm32_v6",
+        "multi_v7_defconfig": "arm32_v7",
+        "malta_defconfig": "mipsel",
+        "ppc44x_defconfig": "ppc32",
+        "pseries_defconfig": "ppc64",
+        "powernv_defconfig": "ppc64le",
+    }
+    if "CONFIG_CPU_BIG_ENDIAN=y" in full_config:
+        if arch == "arm64":
+            return "arm64be"
+        if arch == "mips":
+            return "mips"
+    if base_config in unique_defconfigs:
+        return unique_defconfigs[base_config]
+    if "defconfig" in base_config:
+        return "x86" if arch == "i386" else arch
+    raise RuntimeError("unknown CBL name")
+
+
+def _read_builds():
+    builds = "builds.json"
+    if os.environ.get("MOCK"):
+        builds = "mock.builds.json"
+    try:
+        if pathlib.Path(builds).stat().st_size == 0:
+            raise RuntimeError(f"{builds} is zero sized?")
+        with open(builds, encoding='utf-8') as file:
+            builds = json.load(file)
+    except FileNotFoundError as err:
+        print_red(f"Unable to find {builds}. Artifact not saved?")
+        raise err
+    return builds["builds"].values()
+
+
+def get_requested_llvm_version():
+    ver = os.environ["LLVM_VERSION"]
+    ci_folder = pathlib.Path(__file__).resolve().parent
+    with open(ci_folder.joinpath("LLVM_TOT_VERSION"),
+              encoding='utf-8') as file:
+        llvm_tot_version = str(int(file.read())).strip()
+    return "clang-" + ("nightly" if ver == llvm_tot_version else ver)
+
+
+def show_builds():
+    print_yellow("Available builds:")
+    for build in _read_builds():
+        arch_val = build['target_arch']
+        llvm_version_val = build['toolchain'].split('-', 1)[1]
+        config_val = "+".join(build["kconfig"])
+        print_yellow(
+            f"\tARCH={arch_val} LLVM_VERSION={llvm_version_val} CONFIG={config_val}",
+        )
+
+
+def get_build():
+    arch = os.environ["ARCH"]
+    configs = os.environ["CONFIG"].split("+")
+    llvm_version = get_requested_llvm_version()
+    for build in _read_builds():
+        if build["target_arch"] == arch and \
+           build["toolchain"] == llvm_version and \
+           build["kconfig"] == configs:
+            return build
+    print_red("Unable to find build")
+    show_builds()
+    sys.exit(1)
+
+
 def get_repo_ref(config, tree_name):
     for tree in config["trees"]:
         if tree["name"] == tree_name:
             return tree["git_repo"], tree["git_ref"]
     raise RuntimeError(f"Could not find git repo and ref for {tree_name}?")
+
+
+def get_llvm_versions(config, tree_name):
+    llvm_versions = set()
+    repo, ref = get_repo_ref(config, tree_name)
+    for build in config["builds"]:
+        if build["git_repo"] == repo and build["git_ref"] == ref:
+            llvm_versions.add(build["llvm_version"])
+    return llvm_versions
+
+
+def print_red(msg):
+    print(f"\033[91m{msg}\033[0m", file=sys.stderr)
+    sys.stderr.flush()
+
+
+def print_yellow(msg):
+    print(f"\033[93m{msg}\033[0m", file=sys.stdout)
+    sys.stdout.flush()
+
+
+def patch_series_flag(tree):
+    ci_folder = pathlib.Path(__file__).resolve().parent
+    patches_folder = pathlib.Path("patches").joinpath(tree)
+    patch_files = glob.glob(f"{ci_folder.joinpath(patches_folder)}/*.patch")
+    return f"--patch-series {patches_folder} " if patch_files else ""
